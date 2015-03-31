3009af299ecdded98737bc901bff11a39ca70ae6
SVMBrown <scotb38@gmail.com>
2015-03-31 16:02:49 -0400

    added age prompt, put confirmation into a function, and output the 2 possible birth years

--- exercise3.rb
+++ exercise3.rb
@@ -1,12 +1,16 @@
-name = nil
-confirm = nil
-until confirm == 1
+def confirmed_input(prompt)
 	confirm = nil
-	name = nil
-	puts "What is your name?"
-	until confirm
-		puts "Are you #{ name ||= gets.chomp }? (Y/N)"
-		confirm = ["N", "Y"].find_index(gets.chomp.upcase)
+	until confirm == 1
+		confirm = nil
+		input = nil
+		puts prompt
+		until confirm
+			puts "is #{ input ||= gets.chomp } correct? (Y/N)"
+			confirm = ["N", "Y"].find_index(gets.chomp.upcase)
+		end
 	end
+	input
 end
-puts "Hi #{name}!"
+puts "Hi #{name = confirmed_input("What is your name?")}!"
+puts "You are #{age = confirmed_input("How old are you?").to_i} years old!"
+puts "You were born in #{birth = Time.now.year - age - 1} or #{birth + 1}!"
